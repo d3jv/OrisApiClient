ffedee3dc71102b41e8784831f3b64b0efcf007b
David Štorek <d.storek@seznam.cz>
2025-03-26 11:11:54 +0100

    Fix incorrect method signature

--- IOrisClient.cs
+++ IOrisClient.cs
@@ -1,12 +1,11 @@
 using OrisApi.Models;
-using static OrisApi.Models.OrisUserClubs;
 
 namespace OrisApi;
 
 public interface IOrisClient
 {
     public Task<OrisResponse<OrisAuth>> AuthenticateAsync(string username, string password);
-    public Task<OrisResponse<OrisUserClub>> GetUserClubsAsync(int orisID, DateTime? date = null);
+    public Task<OrisResponse<OrisUserClubs>> GetUserClubsAsync(int orisID, DateTime? date = null);
     public Task<OrisResponse<OrisUser>> GetUserAsync(string rgnum);
 }
 
