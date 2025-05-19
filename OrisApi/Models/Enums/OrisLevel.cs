4b130086ea14ae8d019136bb6b0d1e9b9d1ec98e
David Štorek <d.storek@seznam.cz>
2025-05-19 15:18:30 +0200

    Add all oris list enums

---
+++ OrisApi/Models/Enums/OrisLevel.cs
@@ -0,0 +1,99 @@
+namespace OrisApi.Models.Enums;
+
+public enum OrisLevel
+{
+    /// <summary>
+    /// Mistrovství ČR
+    ///</summary>
+        MCR = 1,
+
+    /// <summary>
+    /// Žebříček A
+    ///</summary>
+        ZA,
+
+    /// <summary>
+    /// Žebříček B
+    ///</summary>
+        ZB,
+
+    /// <summary>
+    /// Oblastní žebříček
+    ///</summary>
+        OZ,
+
+    /// <summary>
+    /// Jednotlivá etapa
+    ///</summary>
+        E,
+
+    /// <summary>
+    /// Ostatní
+    ///</summary>
+        OST,
+
+    /// <summary>
+    /// Český pohár štafet
+    ///</summary>
+        CPS,
+
+    /// <summary>
+    /// Český pohár
+    ///</summary>
+        CP,
+
+    /// <summary>
+    /// Česká liga klubů
+    ///</summary>
+        CLK,
+
+    /// <summary>
+    /// Sprint cup
+    ///</summary>
+        SC,
+
+    /// <summary>
+    /// Oblastní mistrovství
+    ///</summary>
+        OM,
+
+    /// <summary>
+    /// World Ranking Event
+    ///</summary>
+        WRE,
+
+    /// <summary>
+    /// Zimní liga
+    ///</summary>
+        ZL,
+
+    /// <summary>
+    /// Ostatní oficiální
+    ///</summary>
+        OF,
+
+    /// <summary>
+    /// Školení, schůze, semináře
+    ///</summary>
+        S,
+
+    /// <summary>
+    /// Etapový závod
+    ///</summary>
+        ET,
+
+    /// <summary>
+    /// Veteraniáda ČR
+    ///</summary>
+        VET,
+
+    /// <summary>
+    /// Regionální závod
+    ///</summary>
+        R,
+
+    /// <summary>
+    /// Stacionární tréninkový kemp
+    ///</summary>
+        STK,
+}
---
+++ OrisApi/Models/Enums/OrisRegion.cs
@@ -0,0 +1,6 @@
+namespace OrisApi.Models.Enums;
+
+public enum OrisRegion
+{
+    Č, ČR, HA, JČ, JE, JM, M, MSK, P, StČ, VA, VČ, VY, ZČ,
+}
